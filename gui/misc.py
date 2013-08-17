0ab6c1128b8da8e5f72b72cf7e861cdcd5a55b3a
Mattias Münster <mattiasmun@gmail.com>
2013-08-17 14:26:39 +0200

    Update

--- gui/misc.py
+++ gui/misc.py
@@ -70,7 +70,7 @@ def check_scan_command (data_type, cmd, is_first_scan):
             num = cmd
             cmd = ''
         num = eval_operand(num)
-        cmd += locale.format_string('%f', num)
+        cmd += locale.str(num)
 
         if data_type.startswith('int'):
             if not isinstance(num, int):

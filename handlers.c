cb17171b2c45d93f7f8cec4878dcb9a584409643
Sebastian Parschauer <s.parschauer@gmx.de>
2014-05-07 21:40:25 +0200

    handlers: shorten the pointer format
    The pointer format for memory addresses can be shortened if '%lx'
    is used instead of '%p' as the '0x' prefix is removed. This way we
    also don't have the chance to hit '(nil)' output for 0.

--- handlers.c
+++ handlers.c
@@ -74,11 +74,11 @@
 
 /* try to determine the size of a pointer */
 #if ULONGMAX == 4294967295UL
-#define POINTER_FMT "%10p"
+#define POINTER_FMT "%8lx"
 #elif ULONGMAX == 18446744073709551615UL
-#define POINTER_FMT "%14p"
+#define POINTER_FMT "%12lx"
 #else
-#define POINTER_FMT "%14p"
+#define POINTER_FMT "%12lx"
 #endif
 
 bool handler__set(globals_t * vars, char **argv, unsigned argc)
@@ -400,7 +400,7 @@ bool handler__list(globals_t * vars, char **argv, unsigned argc)
                 reading_iterator /* ,MATCHES_AND_VALUES */);
             unsigned long address_ul = (unsigned long)address;
             int region_id = 99;
-            void *match_off = NULL;
+            unsigned long match_off = 0;
             const char *region_type = "??";
             /* get region info belonging to the match -
              * note: we assume the regions list and matches to be sorted
@@ -411,14 +411,14 @@ bool handler__list(globals_t * vars, char **argv, unsigned argc)
                 if (address_ul < region_start + region->size &&
                   address_ul >= region_start) {
                     region_id = region->id;
-                    match_off = (void *)(address_ul - region->load_addr);
+                    match_off = address_ul - region->load_addr;
                     region_type = region_type_names[region->type];
                     break;
                 }
                 np = np->next;
             }
             fprintf(stdout, "[%2u] "POINTER_FMT", %2u + "POINTER_FMT
-                ", %5s, %s\n", i++, address, region_id, match_off,
+                ", %5s, %s\n", i++, address_ul, region_id, match_off,
                 region_type, v);
         }
 	
@@ -729,9 +729,9 @@ bool handler__lregions(globals_t * vars, char **argv, unsigned argc)
 
         fprintf(stderr, "[%2u] "POINTER_FMT", %7lu bytes, %5s, "
                 POINTER_FMT", %c%c%c, %s\n",
-                region->id, region->start, region->size,
+                region->id, (long)region->start, region->size,
                 region_type_names[region->type],
-                (void *)region->load_addr,
+                region->load_addr,
                 region->flags.read ? 'r' : '-',
                 region->flags.write ? 'w' : '-',
                 region->flags.exec ? 'x' : '-',
